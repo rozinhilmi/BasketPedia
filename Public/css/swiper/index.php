aeb31a0a5e7307ce8825c2039e795f281d960cc6
Rizqi05 <72347550+Rizqi05@users.noreply.github.com>
2021-06-18 13:26:15 +0700

    Add files via upload

---
+++ Public/css/swiper/index.php
@@ -0,0 +1,4 @@
+<?php
+$absolute_path = realpath($_SERVER["DOCUMENT_ROOT"])."/BasketPedia";
+require "$absolute_path/app/core/MVC_model.php";
+require "$absolute_path/app/controller/Super_Default_method.php";
---
+++ Public/js/script.js
@@ -0,0 +1,24 @@
+var isClick = false;
+var chart = document.getElementsByClassName('sizeChart')[0];
+const navbar = document.querySelector('.navbar');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    navbar.style.backgroundColor = 'white';
+  } else {
+    navbar.style.backgroundColor = 'rgba(255, 255, 255,0.4)';
+  }
+}
+
+function sizeChart() {
+  if (isClick) {
+    chart.style.display = 'none';
+    isClick = false;
+  } else {
+    chart.style.display = 'block';
+    isClick = true;
+  }
+}
