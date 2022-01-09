14aa6c78a9e0bd551b828b8dbe744e6c0ff17775
rozinhilmi <69004866+rozinhilmi@users.noreply.github.com>
2022-01-09 20:48:22 +0700

    Delete Public directory

--- Public/images/evidence of transfer/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-$absolute_path = realpath($_SERVER["DOCUMENT_ROOT"])."/BasketPedia";
-require "$absolute_path/app/core/MVC_model.php";
-require "$absolute_path/app/controller/Super_Default_method.php";
